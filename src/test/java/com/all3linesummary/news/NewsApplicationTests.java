70737b1f21f903e8580938aa3c00c90f784eae9c
ChangDaeJun <eowns1209@gmail.com>
2023-04-10 18:04:27 +0900

    creat : 첫 커밋

---
+++ src/test/java/com/all3linesummary/news/NewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.all3linesummary.news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
