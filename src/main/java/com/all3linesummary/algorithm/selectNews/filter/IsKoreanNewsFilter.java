be914cc6b58f629b35aaaba397d216ac2976135e
ChangDaeJun <eowns1209@gmail.com>
2023-05-16 16:52:28 +0900

    feat : 한국어 뉴스 필터 구현
    제목에서 한국어 비중이 60% 이상일 때만 한국어로 판정

--- src/main/java/com/all3linesummary/algorithm/selectNews/filter/IsKoreanNewsFilter.java
+++ src/main/java/com/all3linesummary/algorithm/selectNews/filter/IsKoreanNewsFilter.java
@@ -6,7 +6,24 @@ import com.all3linesummary.util.filter.Filter;
 public class IsKoreanNewsFilter implements Filter<SearchedNews> {
 
     @Override
-    public int checkPoint(SearchedNews object) {
-        return 0;
+    public int checkPoint(SearchedNews news) {
+        int koreanCnt = countKorean(news.getTitle());
+        int passNumber = (int) (news.getTitleLength() * 0.6);
+        if(koreanCnt > passNumber) return 0;
+        return FAIL_DIGIT;
+    }
+
+    private int countKorean(String title){
+        int cnt = 0;
+        for(char c : title.toCharArray()){
+            if (isKorean(c)) cnt++;
+        }
+        return cnt;
+    }
+
+    // 한글 유니코드 범위: 가(0xAC00) ~ 힣(0xD7A3)
+    private boolean isKorean(char c) {
+        if (c >= '\uAC00' && c <= '\uD7A3') return true;
+        return false;
     }
 }
